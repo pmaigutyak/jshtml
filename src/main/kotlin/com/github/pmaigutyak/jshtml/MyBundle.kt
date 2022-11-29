80f8fa52faf207df2615da9ba3e69dd8a3c9b738
GitHub Action <action@github.com>
2022-11-29 15:03:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/pmaigutyak/jshtml/MyBundle.kt
+++ src/main/kotlin/com/github/pmaigutyak/jshtml/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pmaigutyak.jshtml
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pmaigutyak/jshtml/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pmaigutyak/jshtml/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pmaigutyak.jshtml.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pmaigutyak.jshtml.MyBundle
 
 class MyApplicationService {
 
